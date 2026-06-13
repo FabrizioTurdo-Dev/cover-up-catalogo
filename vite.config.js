294c97ba61427553f9efeac4583a71691b57375d
FabrizioTurdo-Dev <krathosbabghee@gmail.com>
2026-06-13 14:20:03 -0300

    cambios en vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cover-up-catalogo/' // ← el nombre exacto de tu repo
+  base: './' // ← el nombre exacto de tu repo
 })
